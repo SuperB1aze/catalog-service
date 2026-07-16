4179cc2b62ce1b992429d64a5c9fd1eceb2309a9
superb1aze <romanabrosimov1@gmail.com>
2026-07-16 14:50:30 +0300

    TASK-1003: Подключил БД и провёл миграцию

--- internal/app/config/section/repository.go
+++ internal/app/config/section/repository.go
@@ -8,11 +8,12 @@ type (
 	}
 
 	RepositoryPostgres struct {
-		Address      string        `required:"true" split_words:"true"`
-		Username     string        `required:"true" split_words:"true"`
-		Password     string        `required:"true" split_words:"true"`
-		Name         string        `required:"true" split_words:"true"`
-		ReadTimeout  time.Duration `default:"5s" split_words:"true"`
-		WriteTimeout time.Duration `default:"5s" split_words:"true"`
+		Address        string        `required:"true" split_words:"true"`
+		Username       string        `required:"true" split_words:"true"`
+		Password       string        `required:"true" split_words:"true"`
+		Name           string        `required:"true" split_words:"true"`
+		ReadTimeout    time.Duration `default:"5s" split_words:"true"`
+		WriteTimeout   time.Duration `default:"5s" split_words:"true"`
+		MigrationTable string        `split_words:"true" default:"schema_migrations"`
 	}
 )
