02e7bc81a0aa6d7da1bb92a474bf1703c349adad
superb1aze <romanabrosimov1@gmail.com>
2026-07-26 16:44:24 +0300

    TASK-1005: Реализовал сервис и обработчики

---
+++ internal/app/handler/http/product/product.go
@@ -0,0 +1,139 @@
+package hproduct
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+
+	"github.com/gofrs/uuid"
+	"github.com/gorilla/mux"
+
+	"github.com/SuperB1aze/catalog-service/internal/app/entity"
+	rhandler "github.com/SuperB1aze/catalog-service/internal/app/handler/http"
+	"github.com/SuperB1aze/catalog-service/internal/app/service"
+	"github.com/SuperB1aze/catalog-service/internal/pkg/http/httph"
+)
+
+type handler struct {
+	srv service.Product
+}
+
+func (h *handler) Create(w http.ResponseWriter, r *http.Request) {
+	var req entity.RequestProductCreate
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		httph.HandleError(w, entity.ErrIncorrectParameters)
+		return
+	}
+
+	if err := req.Validate(); err != nil {
+		httph.HandleError(w, err)
+		return
+	}
+
+	product, err := h.srv.Create(r.Context(), req)
+	if err != nil {
+		httph.HandleError(w, err)
+		return
+	}
+
+	resp := entity.ResponseProductCreate{
+		GUID:         product.GUID,
+		Name:         product.Name,
+		Description:  product.Description,
+		Price:        product.Price,
+		CategoryGUID: product.CategoryGUID,
+		CreatedAt:    product.CreatedAt,
+	}
+
+	httph.SendJSON(w, http.StatusCreated, resp)
+}
+
+func (h *handler) Update(w http.ResponseWriter, r *http.Request) {
+	var req entity.RequestProductUpdate
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		httph.HandleError(w, entity.ErrIncorrectParameters)
+		return
+	}
+
+	if err := req.Validate(); err != nil {
+		httph.HandleError(w, err)
+		return
+	}
+
+	vars := mux.Vars(r)
+	guid, err := uuid.FromString(vars["guid"])
+	if err != nil {
+		httph.HandleError(w, entity.ErrIncorrectParameters)
+		return
+	}
+
+	product, err := h.srv.Update(r.Context(), guid, req)
+	if err != nil {
+		httph.HandleError(w, err)
+		return
+	}
+
+	resp := entity.ResponseProductUpdate{
+		GUID:         product.GUID,
+		Name:         product.Name,
+		Description:  product.Description,
+		Price:        product.Price,
+		CategoryGUID: product.CategoryGUID,
+		CreatedAt:    product.CreatedAt,
+		UpdatedAt:    product.UpdatedAt,
+	}
+
+	httph.SendJSON(w, http.StatusOK, resp)
+}
+
+func (h *handler) Delete(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	guid, err := uuid.FromString(vars["guid"])
+	if err != nil {
+		httph.HandleError(w, entity.ErrIncorrectParameters)
+		return
+	}
+
+	if err := h.srv.Delete(r.Context(), guid); err != nil {
+		httph.HandleError(w, err)
+		return
+	}
+
+	httph.SendEmpty(w, http.StatusOK)
+}
+
+func (h *handler) List(w http.ResponseWriter, r *http.Request) {
+	var req entity.RequestProductList
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil && !errors.Is(err, io.EOF) {
+		httph.HandleError(w, entity.ErrIncorrectParameters)
+		return
+	}
+
+	products, err := h.srv.List(r.Context(), req)
+	if err != nil {
+		httph.HandleError(w, err)
+		return
+	}
+
+	resp := entity.ResponseProductList{
+		Data: make([]entity.ResponseProductListItem, 0, len(products)),
+	}
+	for _, product := range products {
+		resp.Data = append(resp.Data, entity.ResponseProductListItem{
+			GUID:         product.GUID,
+			Name:         product.Name,
+			Description:  product.Description,
+			Price:        product.Price,
+			CategoryGUID: product.CategoryGUID,
+			CreatedAt:    product.CreatedAt,
+			UpdatedAt:    product.UpdatedAt,
+		})
+	}
+
+	httph.SendJSON(w, http.StatusOK, resp)
+}
+
+func NewHandler(srv service.Product) rhandler.Product {
+	return &handler{srv: srv}
+}
